d0db210bf0167e42dba0dc2a31095f63be0deb45
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-08-03 22:05:56 -0500

    first commit

---
+++ hasUniqueChars.js
@@ -0,0 +1,10 @@
+
+// Write your code below
+function hasUniqueChars(word) {
+  let uniqueChars = new Set([])
+  for (let i = 0; i < word.length; i++) {
+    uniqueChars.add(word[i])
+  }
+  return uniqueChars.size === word.length
+}
+
